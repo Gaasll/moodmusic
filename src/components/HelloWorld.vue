05f0711158b56175764fdd39742cec05b475c782
Garabet Aslo <gkaslo@kth.se>
2022-11-30 15:46:30 +0100

    adding sidebar + updates

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string;
-}>();
+
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
+    <h1 class="green">An App Swings</h1>
+    <br>
+    <h3 class="green11">
       A change in the weather can be a nice change of mood at times. Other times it
 can be… frustrating. The combination of weather with our emotions can be
 interesting. Does the weather affect our mood? Do we have a “weather type”?
 Can music help?
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      What's next?
+      
     </h3>
   </div>
 </template>
@@ -23,6 +20,7 @@ Can music help?
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
+  font-family: "Lucida Console", "Courier New", monospace;
   top: -10px;
 }
 
---
+++ src/views/Weather.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+
+<template>
+    <main>
+      <TheWelcome />
+    </main>
+  </template>
+  
+  
+  <style>
+  @media (min-width: 1024px) {
+    .weather {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  } 
+  
+  
+  </style>
